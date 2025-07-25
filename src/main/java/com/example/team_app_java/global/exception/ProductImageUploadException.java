a6e4c365d5b84701f1e85409e618df47ebe8451b
이지은 <tori@ryanlab.kr>
2025-07-25 11:00:22 +0900

    공통 에러 처리 삭제

--- src/main/java/com/example/team_app_java/global/exception/ProductImageUploadException.java
+++
@@ -1,7 +0,0 @@
-package com.example.team_app_java.global.exception;
-
-public class ProductImageUploadException extends RuntimeException {
-    public ProductImageUploadException(String message, Throwable cause){
-        super(message, cause);
-    }
-}
