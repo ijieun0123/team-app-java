4c900bfec28945d2279cc19e9886a39bf5855f3d
이지은 <tori@ryanlab.kr>
2025-08-05 12:51:34 +0900

    .env 파일 없을 때도 예외 안 나도록 변경

--- src/main/java/com/example/team_app_java/TeamAppJavaApplication.java
+++ src/main/java/com/example/team_app_java/TeamAppJavaApplication.java
@@ -6,16 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class TeamAppJavaApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
-		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+			System.setProperty("DB_USERNAME", Optional.ofNullable(dotenv.get("DB_USERNAME")).orElse(System.getenv("DB_USERNAME")));
+			System.setProperty("DB_PASSWORD", Optional.ofNullable(dotenv.get("DB_PASSWORD")).orElse(System.getenv("DB_PASSWORD")));
+		} catch (Exception e) {
+			// 로그 찍고 무시 (필요하면)
+			System.out.println("Dotenv 로딩 실패, 환경변수에서 읽기 시도");
+		}
 
 		SpringApplication.run(TeamAppJavaApplication.class, args);
 	}
